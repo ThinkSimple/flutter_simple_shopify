ea6f58536efbefadd07e4a3379ade42bec3778d0
Rupam <rupam@claritytech.io>
2020-10-19 19:45:09 +0530

    checkoutShippingLineUpdate output

--- lib/graphql_operations/mutations/checkout_shipping_line_update.dart
+++ lib/graphql_operations/mutations/checkout_shipping_line_update.dart
@@ -1,6 +1,9 @@
 const String checkoutShippingLineUpdateMutation = r'''
 mutation checkoutShippingLineUpdate($checkoutId : ID!, $shippingRateHandle : String!) {
   checkoutShippingLineUpdate(checkoutId: $checkoutId, shippingRateHandle: $shippingRatehandle) {
+    checkout {
+      id
+    }
     checkoutUserErrors {
       code
       field
