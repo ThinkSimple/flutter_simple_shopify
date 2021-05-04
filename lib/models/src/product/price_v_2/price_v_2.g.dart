2318e979b973aa2d2f3dbdfd5d68f3d78a2eb6d7
sam-parks <parkssa123@gmail.com>
2021-05-03 17:00:20 -0700

    pirce v2

--- lib/models/src/product/price_v_2/price_v_2.g.dart
+++ lib/models/src/product/price_v_2/price_v_2.g.dart
@@ -8,15 +8,8 @@ part of 'price_v_2.dart';
 
 _$_PriceV2 _$_$_PriceV2FromJson(Map<String, dynamic> json) {
   return _$_PriceV2(
-<<<<<<< HEAD
     amount: JsonHelper.amountFromJson(json['amount']),
     currencyCode: json['currencyCode'] as String,
-=======
-    amount: (json['amount'] as num?)?.toDouble(),
-    currencyCode: json['currencyCode'] as String?,
-    currencySymbol: json['currencySymbol'] as String?,
-    formattedPrice: json['formattedPrice'] as String?,
->>>>>>> 2d6263621e135f317a6c1d78b936d8941299f648
   );
 }
 
