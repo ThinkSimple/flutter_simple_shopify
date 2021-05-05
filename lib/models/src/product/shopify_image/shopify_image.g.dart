6b64a2d6817772e087d2b6b4c0c44835d1fe1862
sam-parks <parkssa123@gmail.com>
2021-05-05 11:28:05 -0700

    update shopify image

--- lib/models/src/product/shopify_image/shopify_image.g.dart
+++ lib/models/src/product/shopify_image/shopify_image.g.dart
@@ -8,15 +8,15 @@ part of 'shopify_image.dart';
 
 _$_ShopifyImage _$_$_ShopifyImageFromJson(Map<String, dynamic> json) {
   return _$_ShopifyImage(
-    altText: json['altText'] as String,
     originalSrc: json['originalSrc'] as String,
     id: json['id'] as String,
+    altText: json['altText'] as String?,
   );
 }
 
 Map<String, dynamic> _$_$_ShopifyImageToJson(_$_ShopifyImage instance) =>
     <String, dynamic>{
-      'altText': instance.altText,
       'originalSrc': instance.originalSrc,
       'id': instance.id,
+      'altText': instance.altText,
     };
