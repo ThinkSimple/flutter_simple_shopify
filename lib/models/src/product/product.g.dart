a440b7b0e88b6333fbd142dc0f8fb0013ad44bc2
sam-parks <parkssa123@gmail.com>
2021-05-12 21:46:11 -0700

    update line items yet again

---
+++ lib/models/src/product/product.g.dart
@@ -0,0 +1,64 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'product.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Product _$_$_ProductFromJson(Map<String, dynamic> json) {
+  return _$_Product(
+    collectionList: (json['collectionList'] as List<dynamic>)
+        .map((e) => AssociatedCollections.fromJson(e as Map<String, dynamic>))
+        .toList(),
+    title: json['title'] as String,
+    id: json['id'] as String,
+    availableForSale: json['availableForSale'] as bool,
+    createdAt: json['createdAt'] as String,
+    productVariants: (json['productVariants'] as List<dynamic>)
+        .map((e) => ProductVariant.fromJson(e as Map<String, dynamic>))
+        .toList(),
+    onlineStoreUrl: json['onlineStoreUrl'] as String,
+    productType: json['productType'] as String,
+    publishedAt: json['publishedAt'] as String,
+    tags: (json['tags'] as List<dynamic>).map((e) => e as String).toList(),
+    updatedAt: json['updatedAt'] as String,
+    cursor: json['cursor'] as String,
+    images: (json['images'] as List<dynamic>)
+        .map((e) => ShopifyImage.fromJson(e as Map<String, dynamic>))
+        .toList(),
+    option: (json['option'] as List<dynamic>)
+        .map((e) => Option.fromJson(e as Map<String, dynamic>))
+        .toList(),
+    vendor: json['vendor'] as String,
+    metafields: (json['metafields'] as List<dynamic>)
+        .map((e) => Metafield.fromJson(e as Map<String, dynamic>))
+        .toList(),
+    description: json['description'] as String? ?? '',
+    descriptionHtml: json['descriptionHtml'] as String? ?? '',
+    handle: json['handle'] as String? ?? '',
+  );
+}
+
+Map<String, dynamic> _$_$_ProductToJson(_$_Product instance) =>
+    <String, dynamic>{
+      'collectionList': instance.collectionList,
+      'title': instance.title,
+      'id': instance.id,
+      'availableForSale': instance.availableForSale,
+      'createdAt': instance.createdAt,
+      'productVariants': instance.productVariants,
+      'onlineStoreUrl': instance.onlineStoreUrl,
+      'productType': instance.productType,
+      'publishedAt': instance.publishedAt,
+      'tags': instance.tags,
+      'updatedAt': instance.updatedAt,
+      'cursor': instance.cursor,
+      'images': instance.images,
+      'option': instance.option,
+      'vendor': instance.vendor,
+      'metafields': instance.metafields,
+      'description': instance.description,
+      'descriptionHtml': instance.descriptionHtml,
+      'handle': instance.handle,
+    };
---
+++ lib/models/src/shopify_user/last_incomplete_checkout/last_incomplete_checkout.g.dart
@@ -0,0 +1,43 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'last_incomplete_checkout.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_LastIncompleteCheckout _$_$_LastIncompleteCheckoutFromJson(
+    Map<String, dynamic> json) {
+  return _$_LastIncompleteCheckout(
+    completedAt: json['completedAt'] as String?,
+    createdAt: json['createdAt'] as String?,
+    email: json['email'] as String?,
+    id: json['id'] as String?,
+    currencyCode: json['currencyCode'] as String?,
+    webUrl: json['webUrl'] as String?,
+    totalPriceV2: json['totalPriceV2'] == null
+        ? null
+        : PriceV2.fromJson(json['totalPriceV2'] as Map<String, dynamic>),
+    lineItemsSubtotalPrice: json['lineItemsSubtotalPrice'] == null
+        ? null
+        : PriceV2.fromJson(
+            json['lineItemsSubtotalPrice'] as Map<String, dynamic>),
+    lineItems: json['lineItems'] == null
+        ? null
+        : LineItems.fromJson(json['lineItems'] as Map<String, dynamic>),
+  );
+}
+
+Map<String, dynamic> _$_$_LastIncompleteCheckoutToJson(
+        _$_LastIncompleteCheckout instance) =>
+    <String, dynamic>{
+      'completedAt': instance.completedAt,
+      'createdAt': instance.createdAt,
+      'email': instance.email,
+      'id': instance.id,
+      'currencyCode': instance.currencyCode,
+      'webUrl': instance.webUrl,
+      'totalPriceV2': instance.totalPriceV2,
+      'lineItemsSubtotalPrice': instance.lineItemsSubtotalPrice,
+      'lineItems': instance.lineItems,
+    };
