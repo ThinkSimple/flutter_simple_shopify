aaa92d93c791f3cc8f331aef9507f9eb34926f08
sam-parks <parkssa123@gmail.com>
2021-05-12 20:49:35 -0700

    update checkout

--- lib/models/src/checkout/checkout.g.dart
+++ lib/models/src/checkout/checkout.g.dart
@@ -9,15 +9,10 @@ part of 'checkout.dart';
 _$_Checkout _$_$_CheckoutFromJson(Map<String, dynamic> json) {
   return _$_Checkout(
     id: json['id'] as String,
-    email: json['email'] as String,
     availableShippingrates: json['availableShippingrates'] == null
         ? null
         : AvailableShippingRates.fromJson(
             json['availableShippingrates'] as Map<String, dynamic>),
-    shippingLine:
-        ShippingRates.fromJson(json['shippingLine'] as Map<String, dynamic>),
-    shippingAddress: MailingAddress.fromJson(
-        json['shippingAddress'] as Map<String, dynamic>),
     createdAt: json['createdAt'] as String,
     currencyCode: json['currencyCode'] as String,
     lineItems: LineItems.fromJson(json['lineItems'] as Map<String, dynamic>),
@@ -28,13 +23,23 @@ _$_Checkout _$_$_CheckoutFromJson(Map<String, dynamic> json) {
     taxExempt: json['taxExempt'] as bool,
     subtotalPriceV2:
         PriceV2.fromJson(json['subtotalPriceV2'] as Map<String, dynamic>),
-    orderStatusUrl: json['orderStatusUrl'] as String,
     requiresShipping: json['requiresShipping'] as bool,
-    order: Order.fromJson(json['order'] as Map<String, dynamic>),
     appliedGiftcards: (json['appliedGiftcards'] as List<dynamic>?)
             ?.map((e) => AppliedGiftCards.fromJson(e as Map<String, dynamic>))
             .toList() ??
         [],
+    order: json['order'] == null
+        ? null
+        : Order.fromJson(json['order'] as Map<String, dynamic>),
+    orderStatusUrl: json['orderStatusUrl'] as String?,
+    shippingAddress: json['shippingAddress'] == null
+        ? null
+        : MailingAddress.fromJson(
+            json['shippingAddress'] as Map<String, dynamic>),
+    shippingLine: json['shippingLine'] == null
+        ? null
+        : ShippingRates.fromJson(json['shippingLine'] as Map<String, dynamic>),
+    email: json['email'] as String?,
     completedAt: json['completedAt'] as String?,
     note: json['note'] as String?,
     webUrl: json['webUrl'] as String?,
@@ -45,10 +50,7 @@ _$_Checkout _$_$_CheckoutFromJson(Map<String, dynamic> json) {
 Map<String, dynamic> _$_$_CheckoutToJson(_$_Checkout instance) =>
     <String, dynamic>{
       'id': instance.id,
-      'email': instance.email,
       'availableShippingrates': instance.availableShippingrates,
-      'shippingLine': instance.shippingLine,
-      'shippingAddress': instance.shippingAddress,
       'createdAt': instance.createdAt,
       'currencyCode': instance.currencyCode,
       'lineItems': instance.lineItems,
@@ -57,10 +59,13 @@ Map<String, dynamic> _$_$_CheckoutToJson(_$_Checkout instance) =>
       'taxesIncluded': instance.taxesIncluded,
       'taxExempt': instance.taxExempt,
       'subtotalPriceV2': instance.subtotalPriceV2,
-      'orderStatusUrl': instance.orderStatusUrl,
       'requiresShipping': instance.requiresShipping,
-      'order': instance.order,
       'appliedGiftcards': instance.appliedGiftcards,
+      'order': instance.order,
+      'orderStatusUrl': instance.orderStatusUrl,
+      'shippingAddress': instance.shippingAddress,
+      'shippingLine': instance.shippingLine,
+      'email': instance.email,
       'completedAt': instance.completedAt,
       'note': instance.note,
       'webUrl': instance.webUrl,
