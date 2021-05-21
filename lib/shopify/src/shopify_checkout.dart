04bacc2843bdea64ca90745bbd21581e6c0d5de2
sam-parks <parkssa123@gmail.com>
2021-05-21 09:24:51 -0700

    update checkout mutation

--- lib/shopify/src/shopify_checkout.dart
+++ lib/shopify/src/shopify_checkout.dart
@@ -265,7 +265,7 @@ class ShopifyCheckout with ShopifyError {
         'lineItems': [
           for (var lineItem in lineItems)
             {
-              'variantId': lineItem.id,
+              'variantId': lineItem.variant!.id,
               'quantity': lineItem.quantity,
             }
         ]
