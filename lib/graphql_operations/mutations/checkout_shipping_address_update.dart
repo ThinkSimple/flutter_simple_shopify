b638f87ba42c5f30f8241f7bc236417792334cd9
sam-parks <parkssa123@gmail.com>
2021-07-12 14:26:51 -0400

    shipping address checkout update

--- lib/graphql_operations/mutations/checkout_shipping_address_update.dart
+++ lib/graphql_operations/mutations/checkout_shipping_address_update.dart
@@ -3,20 +3,63 @@ mutation checkoutShippingAddressUpdate($checkoutId : ID!, $address1 : String, $a
   checkoutShippingAddressUpdateV2(checkoutId: $checkoutId, shippingAddress: {address1: $address1, address2: $address2, company: $company, city: $city, country: $country, firstName: $firstName, lastName: $lastName, phone: $phone, province: $province, zip: $zip}) {
     checkout {
       id
-      webUrl
-      lineItems(first: 5) {
-         edges {
-           node {
-             id
-             title
-             quantity
-              discountAllocations {
+      email
+      ready
+      appliedGiftCards {
+        amountUsedV2 {
+          amount
+          currencyCode
+        }
+        balanceV2 {
+          amount
+          currencyCode
+        }
+        id
+      }
+      requiresShipping
+      shippingLine {
+        handle
+        priceV2 {
+          amount
+          currencyCode
+        }
+        title
+      }
+      shippingAddress {
+        address1
+        address2
+        city
+        company
+        country
+        countryCodeV2
+        firstName
+        formattedArea
+        id
+        lastName
+        latitude
+        longitude
+        name
+        phone
+        province
+        provinceCode
+        zip
+      }
+      completedAt
+      createdAt
+      currencyCode
+      lineItems(first: 10) {
+        edges {
+          node {
+            id
+            quantity
+            title
+             discountAllocations {
               allocatedAmount {
                 amount
                 currencyCode
               }
             }
-             variant {
+            variant {
               id
               priceV2 {
                 amount
@@ -40,6 +83,27 @@ mutation checkoutShippingAddressUpdate($checkoutId : ID!, $address1 : String, $a
             }
           }
         }
+      }
+      note
+      webUrl
+      updatedAt
+      totalTaxV2 {
+        amount
+        currencyCode
+      }
+      totalPriceV2 {
+        amount
+        currencyCode
+      }
+      taxesIncluded
+      taxExempt
+      subtotalPriceV2 {
+        amount
+        currencyCode
+      }
+      orderStatusUrl
+      order {
+        id
       }
     }
      checkoutUserErrors {
