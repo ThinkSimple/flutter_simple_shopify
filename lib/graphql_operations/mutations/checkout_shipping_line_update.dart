60cec472c5d1b238a81057aa5d5f7e9fc8b47ad7
sam-parks <parkssa123@gmail.com>
2021-07-12 14:10:56 -0400

    return checkout from shipping line update

--- lib/graphql_operations/mutations/checkout_shipping_line_update.dart
+++ lib/graphql_operations/mutations/checkout_shipping_line_update.dart
@@ -3,20 +3,74 @@ mutation checkoutShippingLineUpdate($checkoutId: ID!, $shippingRateHandle: Strin
   checkoutShippingLineUpdate(checkoutId: $checkoutId, shippingRateHandle: $shippingRateHandle) {
     checkout {
       id
-      webUrl
-      lineItems(first: 5) {
-         edges {
-           node {
-             id
-             title
-             quantity
-              discountAllocations {
+      ready
+      email
+      appliedGiftCards {
+        amountUsedV2 {
+          amount
+          currencyCode
+        }
+        balanceV2 {
+          amount
+          currencyCode
+        }
+        id
+      }
+      requiresShipping
+      shippingLine {
+        handle
+        priceV2 {
+          amount
+          currencyCode
+        }
+        title
+      }
+      availableShippingRates {
+        ready
+        shippingRates {
+          handle
+          title
+          priceV2 {
+            amount
+            currencyCode
+          }
+        }
+      }
+      shippingAddress {
+        address1
+        address2
+        city
+        company
+        country
+        countryCodeV2
+        firstName
+        formattedArea
+        id
+        lastName
+        latitude
+        longitude
+        name
+        phone
+        province
+        provinceCode
+        zip
+      }
+      completedAt
+      createdAt
+      currencyCode
+      lineItems(first: 10) {
+        edges {
+          node {
+            id
+            quantity
+            title
+            discountAllocations {
               allocatedAmount {
                 amount
                 currencyCode
               }
             }
-             variant {
+            variant {
               id
               priceV2 {
                 amount
@@ -40,6 +94,27 @@ mutation checkoutShippingLineUpdate($checkoutId: ID!, $shippingRateHandle: Strin
             }
           }
         }
+      }
+      note
+      webUrl
+      updatedAt
+      totalTaxV2 {
+        amount
+        currencyCode
+      }
+      totalPriceV2 {
+        amount
+        currencyCode
+      }
+      taxesIncluded
+      taxExempt
+      subtotalPriceV2 {
+        amount
+        currencyCode
+      }
+      orderStatusUrl
+      order {
+        id
       }
     }
     checkoutUserErrors {
