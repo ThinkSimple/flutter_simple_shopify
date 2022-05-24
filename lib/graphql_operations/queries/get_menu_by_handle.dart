455f851f10322f4b1a1e484300f9d69ff0cb9a96
Oli Warriner <owarriner92@gmail.com>
2022-05-24 14:55:06 +0100

    Update query

--- lib/graphql_operations/queries/get_menu_by_handle.dart
+++ lib/graphql_operations/queries/get_menu_by_handle.dart
@@ -3,6 +3,14 @@ query($handle : String!){
   menu(handle: $handle) {
     handle
     id
+    items {
+      id
+      items
+      resourceId
+      tags
+      title
+      type
+    }
     itemsCount
     title
   }
