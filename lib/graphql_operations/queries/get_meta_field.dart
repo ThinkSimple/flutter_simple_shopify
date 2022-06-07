23c37408ce31ddf7acdd52b162474866b2e02a0c
Virat Contractor <virat.contractor@clariontechnologies.co.in>
2022-06-07 12:36:28 +0530

    Minor change in query.

--- lib/graphql_operations/queries/get_meta_field.dart
+++ lib/graphql_operations/queries/get_meta_field.dart
@@ -2,11 +2,12 @@ const String get_meta_field = r'''
 query($id : String){
   product(id: $id) {
     metafields(namespace: "wildFox", first: 10) {
-      edges
-      nodes {
-        namespace
-        key
-        value
+      edges {
+        node {
+          namespace
+          key
+          value
+        }
       }
     }
   }
