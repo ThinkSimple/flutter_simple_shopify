65de57970bd059239649b4e13faee91b18435359
Virat Contractor <virat.contractor@clariontechnologies.co.in>
2022-06-27 15:15:04 +0530

    Minor changes.

--- lib/graphql_operations/queries/get_meta_field.dart
+++ lib/graphql_operations/queries/get_meta_field.dart
@@ -1,15 +1,13 @@
 const String getMetaField = r'''
 query ProductMetafields($productId : ID!){
   product(id: $productId) {
-    metafields(namespace: "wildfox", first: 10) {
+    metafields(namespace: "wildfox", first: 50) {
       edges {
         node {
           id
           namespace
           key
           value
-          valueType
-          description
         }
       }
     }
