aef199ecf070980ed9e09f43286b31655e52cffb
Virat Contractor <virat.contractor@clariontechnologies.co.in>
2022-06-27 15:02:29 +0530

    Get metafields count changed to 10.

--- lib/graphql_operations/queries/get_meta_field.dart
+++ lib/graphql_operations/queries/get_meta_field.dart
@@ -1,7 +1,7 @@
 const String getMetaField = r'''
 query ProductMetafields($productId : ID!){
   product(id: $productId) {
-    metafields(namespace: "wildfox", first: 50) {
+    metafields(namespace: "wildfox", first: 10) {
       edges {
         node {
           id
