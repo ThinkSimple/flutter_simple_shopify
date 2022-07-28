52b529fbded0e04127ecfa70ed40ca36958d6be0
Virat Contractor <virat.contractor@clariontechnologies.co.in>
2022-07-28 11:09:10 +0530

    product variant query changes in meta field.

--- lib/graphql_operations/queries/get_meta_field.dart
+++ lib/graphql_operations/queries/get_meta_field.dart
@@ -1,6 +1,26 @@
 const String getMetaField = r'''
 query ProductMetafields($ownerId : ID!){
   product(id: $ownerId) {
+    id
+    title
+    variants(first : 5){
+      edges {
+        node {
+          id
+          title
+          metafields (first : 250, namespace: $namespace){
+            edges {
+              node {
+                id
+                namespace
+                key
+                value
+              }
+            }
+          }
+        }
+      }
+    }
     metafields(namespace: "wildfox", first: 50) {
       edges {
         node {
--- lib/graphql_operations/queries/get_metafileds_from_product.dart
+++ lib/graphql_operations/queries/get_metafileds_from_product.dart
@@ -1,29 +1,15 @@
 const String getMetafieldsFromProductQuery = r'''
 query($handle: String!, $namespace: String!) {
   productByHandle(handle: $handle) {
-    products (first : 5) {
+    metafields(first: 250, namespace: $namespace) {
       edges {
         node {
           id
-          title
-          variants(first : 5){
-            edges {
-              node {
-                id
-                title
-                metafields (first : 250, namespace: $namespace)){
-                  edges {
-                    node {
-                      id
-                      namespace
-                      key
-                      value
-                    }
-                  }
-                }
-              }
-            }
-          }
+          namespace
+          key
+          value
+          valueType
+          description
         }
       }
     }
